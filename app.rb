1032d9ebdc8c0b34a56e454c15a36730cd4bb581
Logan Smith <logsmith06@gmail.com>
2020-01-23 04:01:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,12 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-    get '/' do
+    get '/test' do
       "Hello, World!"
-      x = ""
-      x = gets
-      x
+      
 
   end
-
+  
 
 
 end
